de40e1ee3ffa6b54f421c72e8599f8da7a3dd461
Dharmendra Negi <dharmendra.s.negi96@gmail.com>
2021-07-14 17:38:47 +0530

    changes in update function

--- src/updateUser/index.js
+++ src/updateUser/index.js
@@ -35,10 +35,10 @@ exports.handler = async event => {
   let ExpressionAttributeValues = {};
 
   Object.keys(userDetails).forEach(function(key) {
-    console.log("Key : " + key + ", Value : " + data[key]);
-    updateExpression += ` #${data[key]} = :${data[key]} ,`;
-    ExpressionAttributeNames["#" + data[key]] = data[key];
-    ExpressionAttributeValues[":" + data[key]] = Item[data[key]];
+    console.log("Key : " + key + ", Value : " + userDetails[key]);
+    updateExpression += ` #${userDetails[key]} = :${userDetails[key]} ,`;
+    ExpressionAttributeNames["#" + userDetails[key]] = userDetails[key];
+    ExpressionAttributeValues[":" + userDetails[key]] = Item[userDetails[key]];
   });
   // for (const property in Item) {
   //   updateExpression += ` #${property} = :${property} ,`;
